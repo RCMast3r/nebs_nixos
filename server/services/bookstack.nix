7d5bd986f104c6f4e186d04f77e99e805b792aaa
Ben Hall <rcmast3r1@gmail.com>
2025-07-15 18:34:44 -0400

    erm

---
+++ server/services/bookstack.nix
@@ -0,0 +1,23 @@
+{ config, lib, pkgs, ... }: 
+{
+  options.nebs-bookstack.enable = lib.mkOption {
+    type = lib.types.bool;
+    default = false;
+    description = "Enable or disable nebs bookstack";
+  };
+
+  config = lib.mkIf config.nebs-bookstack.enable {
+    services.bookstack = {
+      hostname = "wiki.yeet";
+      dataDir = "/mnt/data/bookstack";
+      enable = true;
+
+      database = {
+        user = "bookstack";
+        host = "localhost";
+        name = "bookstack";
+        createLocally = true;
+      };
+    };
+  };
+}
