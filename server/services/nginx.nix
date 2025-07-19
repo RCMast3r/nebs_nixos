2524bcbbbf4ad77c9ff9a87c72a0ff207a02d8eb
Ben Hall <rcmast3r1@gmail.com>
2025-07-19 16:20:59 -0400

    adding the rest of my internal services

--- server/services/nginx.nix
+++ server/services/nginx.nix
@@ -9,5 +9,45 @@
         proxyPass = "http://192.168.86.28:8123";
       };
     };
+    "movies.yeet" = {
+      locations."/" = {
+        proxyPass = "http://192.168.86.28:7878";
+      };
+    };
+    
+    "portainer-old.yeet" = {
+      locations."/" = {
+        proxyPass = "http://192.168.86.24:9443";
+      };
+    }; 
+    "portainer.yeet" = {
+      locations."/" = {
+        proxyPass = "http://192.168.86.28:9443";
+      };
+    };
+    
+    "qbit.yeet" = {
+      locations."/" = {
+        proxyPass = "http://192.168.86.28:8081";
+      };
+    };
+    "tv.yeet" = {
+      locations."/" = {
+        proxyPass = "http://192.168.86.28:8989";
+      };
+    };
+    "wg.yeet" = {
+      locations."/" = {
+        proxyPass = "http://192.168.86.28:51821";
+      };
+    };
+    
+
+    "wiki.yeet" = {
+      locations."/" = {
+        proxyPass = "http://localhost:6875";
+      };
+    };
+    
   };
 }
