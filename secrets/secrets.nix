c70b9f32326679e92f8ed382f4fbd125293f0c3b
Ben Hall <rcmast3r1@gmail.com>
2025-07-19 18:19:09 -0400

    added in duckdns token

--- secrets/secrets.nix
+++ secrets/secrets.nix
@@ -1,9 +1,10 @@
 let
   neb = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIEuOj9l1wgMmCx2kZeBpjcSaYif8pCJ6PeYlW31YcdTW neb@nixos";
   ben = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIMOtpIpzZ1PkFItVd2WWapfgIWAQ7ymuqAlYNpGgE+w5 rcmast3r1@gmail.com";
-  users = [ neb ];
+  users = [ neb ben ];
 in 
 {
   "nebs_ssh_key.age".publicKeys = users;
   "nebs-bs-env-file3.age".publicKeys = users;
+  "duckdns-token.age".publicKeys = users;
 }
